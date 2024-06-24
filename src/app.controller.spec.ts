051c70a89935346d63ab66faee654b5d6360fb44
kleiton <kleytonferreira9@gmail.com>
2024-06-23 22:56:56 -0300

    entendo erros no controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,8 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 describe('AppController', () => {
   let appController: AppController;
+
+
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
